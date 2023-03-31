8b5c554fdf7b5cbf36333805207f081177a2f14a
Gabriel Paz <gabriel.paz@snoopconsulting.com>
2023-03-31 20:04:44 -0300

    (feat): ExchangesLogic added

---
+++ Logic/ExchangesLogic.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess;
+using Models;
+
+namespace Logic;
+
+public class ExchangesLogic
+{
+    private readonly ExchangesAccess _exchangesAccess;
+
+    public ExchangesLogic(ExchangesAccess exchangesAccess)
+    {
+        _exchangesAccess = exchangesAccess;
+    }
+
+    public async Task<List<CriptoBase>?> GetUSDT()
+    {
+        return null;
+    }
+}
