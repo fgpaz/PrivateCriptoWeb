ed9a865ab3e65ca5ec313bcc7fe23682c3fbf05d
Gabriel Paz <gabriel.paz@snoopconsulting.com>
2023-03-31 20:04:12 -0300

    (feat): db CRUD added

---
+++ DataAccess/CriptoBaseDBAccess.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using Models.DAOs;
+
+namespace DataAccess;
+
+public class CriptoBaseDBAccess
+{
+    private readonly CriptoDbContext _context;
+
+    public CriptoBaseDBAccess(CriptoDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Create(CriptoBase? criptoBase)
+    {
+        await _context.CriptoBases.AddAsync(criptoBase);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<CriptoBase?> Get(string exchange)
+    {
+        return await _context.CriptoBases.FirstOrDefaultAsync(e=> e.exchange == exchange);
+    }
+
+    public void Update(CriptoBase c)
+    {
+        _context.CriptoBases.Update(c);
+        _context.SaveChanges();
+    }
+
+    public void Delete(CriptoBase c)
+    {
+        _context.CriptoBases.Remove(c);
+        _context.SaveChanges();
+    }
+}
---
+++ DataAccess/ExchangesDBAccess.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.DAOs;
+
+namespace DataAccess;
+
+public class ExchangesDBAccess
+{
+    private readonly CriptoDbContext _context;
+
+    public ExchangesDBAccess(CriptoDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Create(ExchangeRateDAO? exchange)
+    {
+        await _context.ExchangeRates.AddAsync(exchange);
+        _context.SaveChanges();
+    }
+
+    public async Task<ExchangeRateDAO?> Get(int id)
+    {
+        return await _context.ExchangeRates.FirstOrDefaultAsync(e=> e != null && e.Id == id);
+    }
+
+    public void Update(ExchangeRateDAO exchange)
+    {
+        _context.Update(exchange);
+        _context.SaveChanges();
+    }
+
+    public void Delete(ExchangeRateDAO exchange)
+    {
+        _context.Remove(exchange);
+        _context.SaveChanges();
+    }
+}
